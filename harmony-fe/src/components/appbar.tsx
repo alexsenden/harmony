6911cda291154a3599c233dd85aae181047cb143
EddenW <edden.o.wong@gmail.com>
2023-10-04 20:23:28 -0500

    Created AppBar
    Created and added AppBar to homepage
    added global css
    made provisional logo

---
+++ harmony-fe/src/components/appbar.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Image from 'next/image'
+import Divider from '@mui/material/Divider';
+
+export default function HarmonyAppBar() {
+    return (
+    <React.Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar className = 'navBar' position="sticky" >
+          <Toolbar>
+            <Button>
+            <Box
+                component ="img"
+                sx={{
+                    height: 64,
+                    }}
+                    alt="Your logo."
+                    src={"/harmony1.png"}
+            />
+
+            </Button>
+            <Button className = "navButton" color="inherit">Home</Button>
+            <Button className = "navButton" color="inherit">Profile</Button>
+            <Button className = "navButton" color="inherit" >Search</Button>
+            <Divider orientation="vertical" flexItem sx={{ flexGrow: 1 }}/>
+
+            <Button className = "navButton" color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </React.Fragment>
+    );
+  }
