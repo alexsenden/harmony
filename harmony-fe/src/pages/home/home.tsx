475a8214db95a4744f6c443d7196e7cecf3518f0
Edden Wong <32918555+EddenWong@users.noreply.github.com>
2023-10-10 11:24:34 -0500

    Created Navbar and Profile Page (#35)
    Co-authored-by: EddenW <edden.o.wong@gmail.com>

--- harmony-fe/src/pages/home/home.tsx
+++ harmony-fe/src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { Card } from '@mui/material'
+import { Card, Container } from '@mui/material'
 import useHttpRequest, { HttpMethod } from '../../hooks/httpRequest'
+import HarmonyAppBar from '../../components/appbar'
 
 const HomePage = () => {
   const [sendRequest, response, error, loading] = useHttpRequest({
@@ -13,14 +14,17 @@ const HomePage = () => {
   }, [])
 
   return (
-    <Card variant="outlined">
-      <p>This is the homepage. Hello World!</p>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <p>{response?.echo || JSON.stringify(error)}</p>
-      )}
-    </Card>
+    <>
+      <HarmonyAppBar />
+      <Container maxWidth="xl">
+        <h1>This is the homepage. Hello World!</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <p>{response?.echo || JSON.stringify(error)}</p>
+        )}
+      </Container>
+    </>
   )
 }
 
---
+++ harmony-fe/src/pages/profile/index.ts
@@ -0,0 +1,2 @@
+import ProfilePage from './profile'
+export default ProfilePage
