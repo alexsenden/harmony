001103ddbe8c61b6d2d967e5c0df9a9517c978cc
Andrii Provozin <54965144+Developik@users.noreply.github.com>
2023-10-17 21:21:03 -0500

    Code quality fixes and change to the CI

--- harmony-api/__test__/repos/userRepo.test.ts
+++ harmony-api/__test__/repos/userRepo.test.ts
@@ -1,64 +1,68 @@
-import { register } from '../../src/repos/userRepo';
-import { prismaMock } from '../prisma/singleton'; // Import the prismaMock
+import { register } from '../../src/repos/userRepo'
+import { prismaMock } from '../prisma/singleton' // Import the prismaMock
 import { HttpError } from '../../src/models/error/httpError'
 
-
+/*
+    This test interacts with locally deployable DB, and part of the integration testing
+*/
 describe('register function with Singleton', () => {
   it('should create a new user', async () => {
     const userData = {
-        userId: '1',
-        username: 'testuser',
-        password: 'testpassword',
-        firstName: 'John',
-        lastName: 'Doe',
-        active: true,
-        posts: [],
-        likes: [],
-        comments: [],
-        follows: [],
-        followers: [],
-        createdAt: new Date(),
-    };
+      userId: '1',
+      username: 'testuser',
+      password: 'testpassword',
+      firstName: 'John',
+      lastName: 'Doe',
+      active: true,
+      posts: [],
+      likes: [],
+      comments: [],
+      follows: [],
+      followers: [],
+      createdAt: new Date(),
+    }
 
-    prismaMock.user.create.mockResolvedValue(userData);
+    prismaMock.user.create.mockResolvedValue(userData)
 
-    const result = await register(userData);
+    const result = await register(userData)
 
     const comparisonSrc = {
-        username: userData.username, 
-        password: userData.password,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-    };
+      username: userData.username,
+      password: userData.password,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+    }
 
     const comparisonRes = {
-        username: result.username, 
-        password: result.password,
-        firstName: result.firstName,
-        lastName: result.lastName,
-    };
+      username: result.username,
+      password: result.password,
+      firstName: result.firstName,
+      lastName: result.lastName,
+    }
 
-    expect(comparisonSrc).toEqual(comparisonRes);
-  });
-  
+    expect(comparisonSrc).toEqual(comparisonRes)
+  })
+
   it('should throw an error if username already exists', async () => {
     const userData = {
-        userId: '1',
-        username: 'testuser',
-        password: 'testpassword',
-        firstName: 'John',
-        lastName: 'Doe',
-        active: true,
-        posts: [],
-        likes: [],
-        comments: [],
-        follows: [],
-        followers: [],
-        createdAt: new Date(),
-    };
-    
-    prismaMock.user.create.mockImplementation();
+      userId: '1',
+      username: 'testuser',
+      password: 'testpassword',
+      firstName: 'John',
+      lastName: 'Doe',
+      active: true,
+      posts: [],
+      likes: [],
+      comments: [],
+      follows: [],
+      followers: [],
+      createdAt: new Date(),
+    }
 
-    await expect(register(userData)).rejects.toThrow(new HttpError('Username already exists', 400));
-  });
-});
+    prismaMock.user.create.mockImplementation()
+
+    await expect(register(userData)).rejects.toThrow(
+      new HttpError('Username already exists', 400)
+    )
+  })
+})
