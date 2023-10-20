bf5de7d2b711b066050350f9af68295bd8b66e82
Andrii Provozin <54965144+Developik@users.noreply.github.com>
2023-10-19 23:51:22 -0500

    Added fixed tests
    Accepted PR was sitting out for a while.
    Wanted to work on further testing.

--- harmony-api/__test__/repos/userRepo.test.ts
+++ harmony-api/__test__/repos/userRepo.test.ts
@@ -1,12 +1,28 @@
 import { register } from '../../src/repos/userRepo'
 import { prismaMock } from '../prisma/singleton' // Import the prismaMock
-//import { HttpError } from '../../src/models/error/httpError'
+import { HttpError } from '../../src/models/error/httpError'
 import prisma from '../../prisma/prisma'
 
 /*
     This test interacts with locally deployable DB, and part of the integration testing
 */
+
 describe('register function with Singleton', () => {
+  const userData = {
+    userId: '1',
+    username: 'testuser',
+    password: 'testpassword',
+    firstName: 'John',
+    lastName: 'Doe',
+    active: true,
+    posts: [],
+    likes: [],
+    comments: [],
+    follows: [],
+    followers: [],
+    createdAt: new Date(),
+  }
+
   it('should create a new user', async () => {
     jest.spyOn(prisma.user, 'create').mockResolvedValueOnce({
       userId: '1',
@@ -17,21 +33,6 @@ describe('register function with Singleton', () => {
       active: true,
       createdAt: new Date(),
     })
-
-    const userData = {
-      userId: '1',
-      username: 'testuser',
-      password: 'testpassword',
-      firstName: 'John',
-      lastName: 'Doe',
-      active: true,
-      posts: [],
-      likes: [],
-      comments: [],
-      follows: [],
-      followers: [],
-      createdAt: new Date(),
-    }
 
     prismaMock.user.create.mockResolvedValue(userData)
 
@@ -53,29 +54,11 @@ describe('register function with Singleton', () => {
 
     expect(comparisonSrc).toEqual(comparisonRes)
   })
-  /*
+
   it('should throw an error if username already exists', async () => {
-    const userData = {
-      userId: '1',
-      username: 'testuser',
-      password: 'testpassword',
-      firstName: 'John',
-      lastName: 'Doe',
-      active: true,
-      posts: [],
-      likes: [],
-      comments: [],
-      follows: [],
-      followers: [],
-      createdAt: new Date(),
-    }
+    prismaMock.user.create.mockResolvedValue(userData)
 
-    prismaMock.user.create.mockImplementation()
-
-    await expect(register(userData)).rejects.toThrow(
-      new HttpError('Username already exists', 400)
-    )
+    await register(userData)
+    await expect(register(userData)).rejects.toThrow(HttpError)
   })
-  TEMPORARILY DISABLED.
-  */
 })
