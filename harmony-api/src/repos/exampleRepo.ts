ca523ee3035fd1292132cd8b1e735d29ecc66663
alexsenden <alexandersenden@gmail.com>
2023-10-31 13:29:48 -0500

    Remove example content in API

--- harmony-api/src/repos/exampleRepo.ts
+++
@@ -1,5 +0,0 @@
-import { Example } from '../models/example'
-
-export const getExample = async (echo?: string): Promise<Example> => {
-  return { echo }
-}
--- harmony-api/src/services/exampleService.ts
+++
@@ -1,6 +0,0 @@
-import * as exampleRepo from '../repos/exampleRepo'
-import { Example } from '../models/example'
-
-export const getExample = async (echo?: string): Promise<Example> => {
-  return exampleRepo.getExample(echo)
-}
