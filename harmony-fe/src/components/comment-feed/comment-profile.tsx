b9806fb2beadb87693fbfc7195e694604f14727a
Edden Wong <32918555+EddenWong@users.noreply.github.com>
2023-11-06 15:35:13 -0600

    Added time to profile comments tab

--- harmony-fe/src/components/comment-feed/comment-profile.tsx
+++ harmony-fe/src/components/comment-feed/comment-profile.tsx
@@ -10,6 +10,7 @@ import {
 
 import { CommentWithUser } from '../../models/comment'
 import TextBlock from '../text-block'
+import moment from 'moment'
 
 interface CommentProps {
   comment: CommentWithUser
@@ -18,26 +19,30 @@ interface CommentProps {
 const Comment = ({ comment }: CommentProps) => {
   return (
     <Card variant="outlined" sx={{ mb: 1 }}>
-      <Stack direction="row">
-        <CardContent>
-          <TextBlock gutterBottom variant="body1">
-            <Box>
-              <Button href={`/profile/${comment.user.username}`}>
-                <Avatar
-                  src={`/images/profilepic/${comment.user.picture}.png`}
-                  sx={{ mr: 1, height: 24, width: 24 }}
-                ></Avatar>
-                <strong>{comment.user.username}</strong>
-              </Button>
-              <>on </>
-              <Link href={`/posts/${comment.postId}`} underline="none">
-                {comment.postTitle}
-              </Link>
-            </Box>
-          </TextBlock>
-          <TextBlock>{comment.content}</TextBlock>
-        </CardContent>
-      </Stack>
+      <CardContent>
+        <TextBlock
+          display="flex"
+          justifyContent="space-between"
+          gutterBottom
+          variant="body1"
+        >
+          <Box>
+            <Button href={`/profile/${comment.user.username}`}>
+              <Avatar
+                src={`/images/profilepic/${comment.user.picture}.png`}
+                sx={{ mr: 1, height: 24, width: 24 }}
+              ></Avatar>
+              <strong>{comment.user.username}</strong>
+            </Button>
+            <>on </>
+            <Link href={`/posts/${comment.postId}`} underline="none">
+              {comment.postTitle}
+            </Link>
+          </Box>
+          {moment(comment.createdAt).fromNow()}
+        </TextBlock>
+        <TextBlock>{comment.content}</TextBlock>
+      </CardContent>
     </Card>
   )
 }
