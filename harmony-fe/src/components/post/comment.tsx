e8213fc0814becae13553a50eefdd593171cbc45
Ikram Khan <33215354+moiikram@users.noreply.github.com>
2023-11-05 21:37:08 -0600

    - added posted time on comments
    - comments also display how long ago they were posted.

--- harmony-fe/src/components/post/comment.tsx
+++ harmony-fe/src/components/post/comment.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Button, CardContent } from '@mui/material'
-
+import { Avatar, Box, Button, CardContent } from '@mui/material'
+import moment from 'moment'
 import { CommentWithUser } from '../../models/comment'
 import TextBlock from '../text-block'
+import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded'
 
 interface CommentProps {
   comment: CommentWithUser
@@ -10,7 +11,12 @@ interface CommentProps {
 const Comment = ({ comment }: CommentProps) => {
   return (
     <CardContent>
-      <TextBlock gutterBottom variant="body1">
+      <TextBlock
+        display="flex"
+        justifyContent="space-between"
+        gutterBottom
+        variant="body1"
+      >
         <Button href={`/profile/${comment.user.username}`}>
           <Avatar
             src={`/images/profilepic/${comment.user.picture}.png`}
@@ -18,8 +24,12 @@ const Comment = ({ comment }: CommentProps) => {
           ></Avatar>
           <strong>{comment.user.username}</strong>
         </Button>
+        {moment(comment.createdAt).fromNow()}
       </TextBlock>
-      <TextBlock>{comment.content}</TextBlock>
+      <Box display="flex" justifyContent="space-between">
+        <TextBlock>{comment.content}</TextBlock>
+        <MoreHorizRoundedIcon></MoreHorizRoundedIcon>
+      </Box>
     </CardContent>
   )
 }
